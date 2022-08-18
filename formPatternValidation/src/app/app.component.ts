897eaaaaed93cd613b57dd55a3fca76460043609
hariom <>
2022-08-18 10:56:42 +0530

    address controls

--- formPatternValidation/src/app/app.component.ts
+++ formPatternValidation/src/app/app.component.ts
@@ -10,18 +10,46 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 export class AppComponent {
   testFormGrp;
   usernamePattern = "^[a-zA-Z]*$";
+  phoneNumberPattern = "^[0-9]*$";
+  isSubmitted : boolean = false;
+
   ngOnInit() {
+    this.isSubmitted = false;
     this.testFormGrp = new FormGroup({
-      username : new FormControl('',[
-        Validators.required,Validators.pattern(this.usernamePattern)])
-    })
+      username : new FormControl('',[Validators.required,Validators.pattern(this.usernamePattern)]),
+      email : new FormControl('',[Validators.required,Validators.email]),
+      password : new FormControl('',[Validators.required]),
+      phone : new FormControl('',[Validators.required,Validators.pattern(this.phoneNumberPattern)]),
+      address : new FormControl('',[Validators.required]),
+      })
   }
 
 get username() {
   return this.testFormGrp.get('username');
 } 
 
+get email() {
+  return this.testFormGrp.get('email');
+} 
+
+get password() {
+  return this.testFormGrp.get('password');
+} 
+
+get phone() {
+  return this.testFormGrp.get('phone');
+} 
+
+get address() {
+  return this.testFormGrp.get('address');
+} 
+
   onSubmit() {
+    console.log(this.testFormGrp);
     console.log(this.username.valid);
+    console.log(this.email.valid);
+    console.log(this.phone.valid);
+    console.log(this.password.valid);
+    console.log(this.address.valid);
   }
 }
